91210977b4f41a399673ac5e42af9f85210b9474
njh08d <njh08d@gmail.com>
2015-03-25 02:16:39 -0400

    Setting up

--- app/src/main/java/com/ezsnips/ez_snips/MainActivity.java
+++ app/src/main/java/com/ezsnips/ez_snips/MainActivity.java
@@ -1,12 +1,13 @@
 package com.ezsnips.ez_snips;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.graphics.Color;
-import android.database.sqlite.SQLiteDatabase.CursorFactory;
-
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
@@ -14,28 +15,41 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        String listItem []={"Login","Create User Account","Department  Details"};
+        ListView lvMain=(ListView)findViewById(R.id.lv_main);
+        lvMain.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, listItem));
+
+        lvMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+                                    long arg3) {
+
+                if(arg2==0)
+                {
+                    Intent i=new Intent(MainActivity.this,LoginActivity.class);
+                    startActivity(i);
+                }
+                else if(arg2==1)
+                {
+                    Intent i=new Intent(MainActivity.this,CreateUserActivity.class);
+                    startActivity(i);
+                }
+                else if(arg2==2)
+                {
+                    Intent i=new Intent(MainActivity.this,DeptActivity.class);
+                    startActivity(i);
+                }
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
